056c2095b3bb776bb4556adc620a1cf501fc60a3
Guanhui Guan <guanhui.guan@columbia.edu>
2017-12-25 11:34:54 -0500

    Go all the way left; Change right and left children

---
+++ Binary Tree Upside Down.java
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode upsideDownBinaryTree(TreeNode root) {
+        // goes left; lchild.left = rchild, lchild.right = parent, disconnects original left, right
+        if(root == null || root.left == null)   return root;
+        
+        TreeNode reNode = upsideDownBinaryTree(root.left);
+        root.left.left = root.right;
+        root.left.right = root;
+        root.left = null;
+        root.right = null;
+        return reNode;
+    }
+}
