d9713c5be56b23a4b65a46ea54d94905ae1e3d8b
Guanhui Guan <guanhui.guan@columbia.edu>
2018-01-07 09:49:32 +0800

    MainClass with tree construction methods

--- MainClass.java
+++ MainClass.java
@@ -9,12 +9,20 @@ public class MainClass {
     	BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
     	String line;
     	while((line = in.readLine()) != null) {
-    		int[] nums = string2Array(line);
+//    		int[] nums = string2Array(line);
+//    		line = in.readLine();
+//    		int target = Integer.parseInt(line);
+//    		int[] outData = new Solution().twoSum(nums, target);
+//        	String output = array2String(outData);
+//            System.out.println(output);
+    		
+    		TreeNode p = string2Tree(line);
     		line = in.readLine();
-    		int target = Integer.parseInt(line);
-    		int[] outData = new Solution().twoSum(nums, target);
-        	String output = array2String(outData);
-            System.out.println(output);
+    		TreeNode q = string2Tree(line);
+    		boolean out = new Solution().isSameTree(p, q);
+//    		String output = Integer.toString(out);
+    		System.out.println(out);
+    		
     	}
     }
     
@@ -42,6 +50,34 @@ public class MainClass {
     	//    	delete the last ", "
     	re = re.substring(0, re.length()-2) + "]";
     	return re;
+    }
+    
+    public static String[] string2SArray(String line) {
+    	line = line.trim();
+    	line = line.substring(1, line.length()-1);
+    	String[] parts = line.split(",");
+    	
+    	for(int i = 0; i < parts.length; i++) {
+    		parts[i] = parts[i].trim();
+    		parts[i] = parts[i].substring(1, parts[i].length()-1);
+    	}
+    	return parts;
+    }
+    
+    public static TreeNode string2Tree(String line) {
+    	line = line.trim();
+    	line = line.substring(1, line.length()-1);
+    	String[] parts = line.split(",");
+    	TreeNode root = sArray2Tree(parts, 0);
+    	return root;
+    }
+    
+    public static TreeNode sArray2Tree(String[] parts, int ind) {
+    	if(ind >= parts.length || parts[ind].equals("null"))	return null;
+    	TreeNode node = new TreeNode(Integer.parseInt(parts[ind]));
+    	node.left = sArray2Tree(parts, 2*ind+1);
+    	node.right = sArray2Tree(parts, 2*ind+2);
+    	return node;
     }
     
 }
