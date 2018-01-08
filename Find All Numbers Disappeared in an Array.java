3487f1cf6786eeb3cf19252a2a25f9d0748cb1a0
Guanhui Guan <guanhui.guan@columbia.edu>
2018-01-09 00:32:10 +0800

    Swap to right index

--- Find All Numbers Disappeared in an Array.java
+++ Find All Numbers Disappeared in an Array.java
@@ -1,20 +1,21 @@
 class Solution {
     public List<Integer> findDisappearedNumbers(int[] nums) {
         int n = nums.length;
-        List<Integer> re = new ArrayList<>();
-        // every time value is read, nums[value] is marked negative
-        for(int num: nums){
-            int value = Math.abs(num) - 1;
-            if(nums[value] > 0){
-                nums[value] = -nums[value];
+        for(int i = 0; i < n; i++) {
+            while(nums[nums[i]-1] != nums[i]) {
+                swap(nums, nums[i]-1, i);
             }
         }
-        // those positive are not marked in the first loop
-        for(int i = 0; i < n; i++){
-            if(nums[i] > 0){
-                re.add(i+1);
-            }
+        List<Integer> list = new ArrayList<>();
+        for(int i = 0; i < n; i++) {
+            if(nums[i] != i+1)  list.add(i+1);
         }
-        return re;
+        return list;
+    }
+    
+    public void swap(int[] nums, int x1, int x2) {
+        int t = nums[x1];
+        nums[x1] = nums[x2];
+        nums[x2] = t;
     }
 }
