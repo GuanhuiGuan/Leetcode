98de45a5bb871cc39dec01b48318343c1daa99ca
Guanhui Guan <guanhui.guan@columbia.edu>
2018-01-08 19:23:30 +0800

    2 lists, go node.next.next

---
+++ Odd Even Linked List.java
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        if(head == null)    return head;
+        ListNode odd = head, even = head.next, evenH = even;
+        while(even != null && even.next != null) {
+            odd.next = odd.next.next;
+            odd = odd.next;
+            even.next = even.next.next;
+            even = even.next;
+        }
+        odd.next = evenH;
+        return head;
+    }
+}
