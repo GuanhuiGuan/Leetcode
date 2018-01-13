918692efed321137ca4d98fc9b42a1530c984eea
Guanhui Guan <guanhui.guan@columbia.edu>
2018-01-12 21:13:17 -0500

    Math, GDC

---
+++ Water and Jug Problem.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean canMeasureWater(int x, int y, int z) {
+        if(z == 0)  return true;
+        if(x==0 || y==0)    return x==0? y==z: x==z;
+        if(x+y < z) return false;
+        // Baczout's identity, k*GCD = k(ax + by)
+        return z%GCD(x, y) == 0;
+    }
+    
+    public int GCD(int x, int y) {
+        while(y != 0) {
+            int t = y;
+            y = x%y;
+            x = t;
+        }
+        return x;
+    }
+}
