ad954c66b21dad9ba697588127891dd9ee0af429
Guanhui Guan <guanhui.guan@columbia.edu>
2018-01-14 02:09:32 -0500

    DP/Record each letter positions/Use treeset ceiling/floor/lower

--- Count Different Palindromic Subsequences.java
+++ Count Different Palindromic Subsequences.java
@@ -1,57 +1,42 @@
-/* REF: https://discuss.leetcode.com/topic/111483/java-96ms-dp-solution-with-detailed-explanation
-*/
-
-
 class Solution {
+    // count palindromes based on letters
+    int cons = (int)Math.pow(10, 9)+7;
+    Integer[][] dp;
+    
     public int countPalindromicSubsequences(String S) {
-        char[] str = S.toCharArray();
+        TreeSet[] letters = new TreeSet[26];
         int n = S.length();
-        // #palimdromic subsequences in substring st to end
-        int[][] dp = new int[n][n];
-        
-        // one char is a PS
-        for(int i = 0; i < n; i++){
-            dp[i][i] = 1;
+        dp = new Integer[n+1][n+1];
+        for(int i = 0; i < 26; i++) {
+            letters[i] = new TreeSet<Integer>();
         }
         
-        // dist: distance between st and end
-        for(int dist = 1; dist < n; dist++){
-            for(int st = 0; st+dist < n; st++){
-                int end = st+dist;
-                
-                // not the same
-                if(str[st] != str[end]){
-                    dp[st][end] = dp[st+1][end] + dp[st][end-1] - dp[st+1][end-1];
-                }
-                // same
-                else{
-                    // remove duplicates
-                    int low = st+1;
-                    int high = end-1;
-                    while(low <= high && str[low] != str[st]){
-                        low++;
-                    }
-                    while(low <= high && str[high] != str[end]){
-                        high--;
-                    }
-                    
-                    // e.g. "aba", b counts b and aba, a counts a and aa
-                    if(low > high){
-                        dp[st][end] = 2*dp[st+1][end-1] + 2;
-                    }
-                    // e.g. "aaa", a in mid counts a and aaa, others count aa
-                    else if(low == high){
-                        dp[st][end] = 2*dp[st+1][end-1] + 1;
-                    }
-                    // e.g. "aabaa", aba counts a, b, aa, aba, all counts aaa, aba, aaaa, aabaa
-                    // aba is redundant
-                    else{
-                        dp[st][end] = 2*dp[st+1][end-1] - dp[low+1][high-1];
-                    }
-                }
-                dp[st][end] = dp[st][end] < 0 ? dp[st][end] + 1000000007 : dp[st][end]%(1000000007);
-            }
+        for(int i = 0; i < n; i++) {
+            // add positions of each letter
+            Integer c = S.charAt(i) - 'a';
+            letters[c].add(i);
         }
-        return dp[0][n-1];
+        
+        // end is out of bound
+        return count(0, n, letters);
+    }
+    
+    public int count(int start, int end, TreeSet<Integer>[] letters) {
+        if(start >= end) return 0;
+        if(dp[start][end] != null)  return dp[start][end];
+        
+        long sum = 0;
+        for(int i = 0; i < 26; i++) {
+            // search inwards
+            Integer nextStart = letters[i].ceiling(start);
+            Integer nextEnd = letters[i].lower(end);    // because end is out of bound
+            if(nextStart == null || nextStart >= end)    continue;
+            sum++;   // 1 char is a pld
+            if(nextStart != nextEnd)    sum++;   // 2 char pld
+            // keep searching inwards(end already decremented)
+            sum += count(nextStart+1, nextEnd, letters);
+        }
+        dp[start][end] = (int)(sum%(cons));
+        return dp[start][end];
     }
 }
