8a0ae2796477df3f04be34dc2bbb2aa0ccd3bde8
Guanhui Guan <guanhui.guan@columbia.edu>
2018-01-16 00:21:22 -0500

    0/1 Knapsack

--- Partition Equal Subset Sum.java
+++ Partition Equal Subset Sum.java
@@ -1,56 +1,20 @@
 class Solution {
     public boolean canPartition(int[] nums) {
-        int sum = 0;
-        for(int n: nums){
-            sum += n;
-        }
+        int sum = 0, n = nums.length;
+        for(int num: nums) sum += num;
         if(sum%2 != 0)  return false;
-        int target = sum/2;
+        sum /= 2;
         
-        boolean[][] dp = new boolean[nums.length+1][target+1];
-        for(int i = 0; i < dp.length; i++){
-            Arrays.fill(dp[i], false);
-        }
-        dp[0][0] = true;
+        // 0/1 knapsack
+        boolean[][] dp = new boolean[n+1][sum+1];
+        for(int i = 0; i <= n; i++) dp[i][0] = true;
+        for(int j = 1; j <= sum; j++)   dp[0][j] = false;
         
-        for(int i = 1; i <= nums.length; i++){
-            for(int j = target; j >= 0; j--){
-                dp[i][j] = dp[i-1][j];
-                if(j >= nums[i-1]){
-                    // pick it or not pick it
-                    dp[i][j] = dp[i-1][j-nums[i-1]] || dp[i][j];
-                }
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= sum; j++) {
+                dp[i][j] = dp[i-1][j] || j-nums[i-1]>=0 && dp[i-1][j-nums[i-1]];
             }
         }
-        return dp[nums.length][target];
-    }
-}
-
-
-// space optimized_______________
-
-class Solution {
-    public boolean canPartition(int[] nums) {
-        int sum = 0;
-        for(int n: nums){
-            sum += n;
-        }
-        if(sum%2 != 0)  return false;
-        int target = sum/2;
-        
-        
-        boolean[] dp = new boolean[target+1];
-        Arrays.fill(dp, false);
-        dp[0] = true;
-        for(int i = 0; i < nums.length; i++){
-            for(int j = target; j >= 0; j--){
-                if(nums[i] <= j){
-                    // not pick || pick
-                    dp[j] = dp[j] || dp[j - nums[i]];
-                }
-            }
-        }
-        
-        return dp[target];
+        return dp[n][sum];
     }
 }
