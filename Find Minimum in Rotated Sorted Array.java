a2c0f5fda6deb7c8924be970c60c76d56c869c7b
Guanhui Guan <guanhui.guan@columbia.edu>
2018-01-16 14:34:34 -0500

    BS, initially max at end, cmp with end

---
+++ Find Minimum in Rotated Sorted Array.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int findMin(int[] nums) {
+        int n = nums.length;
+        int start = 0, end = n-1;
+        while(start < end) {
+            int mid = start + (end-start)/2;
+            if(nums[mid] > nums[end])   start = mid+1;
+            else    end = mid;
+        }
+        return nums[start];
+    }
+}
