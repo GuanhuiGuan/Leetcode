ba4035f8f8f9c3249ea20f04dc50b6694b61c307
Guanhui Guan <guanhui.guan@columbia.edu>
2018-01-17 17:20:50 -0500

    Binary Search, consider overflow

--- Valid Perfect Square.java
+++ Valid Perfect Square.java
@@ -1,14 +1,13 @@
 class Solution {
     public boolean isPerfectSquare(int num) {
-        int left = 1, right = num;
-        while(left <= right){
-            // bit manupulation to divide by 2
-            // long to avoid overflow
-            long mid = (left + right)>>>1;
-            long sq = mid*mid;
-            if(sq == num)  return true;
-            if(sq > num)    right = (int) mid-1;
-            else    left = (int) mid+1;
+        if(num < 0)    return false;
+        int lo = 0, hi = num;
+        while(lo <= hi) {
+            long mid = lo + (hi-lo)/2;
+            long mul = mid*mid;
+            if(mul == num)  return true;
+            if(mul > num)   hi = (int)mid-1;
+            else    lo = (int)mid+1;
         }
         return false;
     }
