b8ba6e16d3143c9673ceafc452485409944420b2
Guanhui Guan <guanhui.guan@columbia.edu>
2018-01-18 15:19:53 -0500

    1-way/2-way BFS; use Set

--- Word Ladder.java
+++ Word Ladder.java
@@ -1,55 +1,84 @@
 class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        // two end BFS
-        Set<String> wordSet = new HashSet<>(wordList), sS = new HashSet<>(), eS = new HashSet<>(), visited = new HashSet<>();
-        sS.add(beginWord);
-        if(wordSet.contains(endWord))   eS.add(endWord);
-        // endWord maybe not in wordList
+        // 2-way BFS, swap start with end if end size < start
+        Set<String> ss = new HashSet<>(), se = new HashSet<>();
+        Set<String> used = new HashSet<>(), ws = new HashSet<>(wordList);
         
+        if(!ws.contains(endWord))   return 0;
+        ss.add(beginWord);
+        se.add(endWord);
         
-        //start word counts as one
+        // ss to se is at least 1 step away
         int len = 1;
-        
-        // sS would be updated per iteration, if one set is empty, can't find
-        while(!sS.isEmpty() && !eS.isEmpty()){
-            // let start set be the smaller one
-            if(sS.size() > eS.size()){
-                Set<String> t = sS;
-                sS = eS;
-                eS = t;
+        while(!ss.isEmpty() && !se.isEmpty()) {
+            // swap if size check failed
+            if(ss.size() > se.size()) {
+                Set<String> temp = ss;
+                ss = se;
+                se = temp;
             }
             
-            // new set for next update of sS
-            Set<String> temp = new HashSet<>();
-            for(String word: sS){
-                char[] cArr = word.toCharArray();
-                for(int i = 0; i < cArr.length; i++){
-                    // save current letter to change it back
-                    char save = cArr[i];
-                    for(char c = 'a'; c <= 'z'; c++){
-                        // not the same word
-                        if(save == c)   continue;
+            // new set to save new found words, swap with ss at the end of this lvl
+            Set<String> next = new HashSet<>();
+            for(String s: ss) {
+                
+                
+                
+                char[] cs = s.toCharArray();
+                for(int i = 0; i < s.length(); i++) {
+                    char t = cs[i];
+                    for(char c = 'a'; c <= 'z'; c++) {
+                        if(c == t)  continue;
+                        cs[i] = c;
+                        String ns = String.valueOf(cs);
                         
-                        cArr[i] = c;
-                        String newWord = String.valueOf(cArr);
-                        
-                        // two sets connect
-                        if(eS.contains(newWord))    return len+1;
-                        // not visited but in wordlist
-                        if(!visited.contains(newWord) && wordSet.contains(newWord)){
-                            visited.add(newWord);
-                            temp.add(newWord);
-                        }
-                        
-                        cArr[i] = save;
+                        // one extra step has been made in this double loop
+                        if(se.contains(ns))    return len+1;
+                        // in ws and not in used
+                        if(ws.contains(ns) && used.add(ns))   next.add(ns);
                     }
+                    cs[i] = t;
                 }
             }
-            // update sS and len of found words
-            sS = temp;
+            // move on to next lvl
+            ss = next;
             len++;
         }
-        // one set is empty; BFS failed
         return 0;
     }
 }
+
+// // one way BFS
+// class Solution {
+//     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+//         Set<String> used = new HashSet<>(), ws = new HashSet<>(wordList); // set wordlist saves time
+//         int count = 1; // when return, count is still at last iteration, thus initial set to 1
+//         Queue<String> q = new LinkedList<>();
+        
+//         q.offer(beginWord);
+//         while(!q.isEmpty()) {
+//             // visit all in this lvl in order to count lvls to find target
+//             for(int k = q.size(); k > 0; k--) {
+//                 String s = q.poll();
+//                 if(s.equals(endWord))   return count;
+                
+//                 // check every char of word, for 26 letters
+//                 char[] cs = s.toCharArray();
+//                 for(int i = 0; i < s.length(); i++) {
+//                     char c = cs[i];
+//                     for(char nc = 'a'; nc <= 'z'; nc++) {
+//                         if(nc == c) continue;
+//                         cs[i] = nc;
+//                         // use valueOf() to merge them into string, otherwise toString() keeps commas
+//                         String ns = String.valueOf(cs);
+//                         // in list and not visited
+//                         if(ws.contains(ns) && used.add(ns))   q.offer(ns);
+//                     }
+//                     cs[i] = c;
+//                 }
+//             }
+//             count++;
+//         }
+//         return 0;
+//     }
+// }
