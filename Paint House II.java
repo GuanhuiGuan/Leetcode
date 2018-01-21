c2fe78ce70750e807105b7e1a912782a95246aab
Guanhui Guan <guanhui.guan@columbia.edu>
2018-01-20 19:03:52 -0500

    DP/ 4 ptrs, 2 last round 2 this round(min1, min2)

--- Paint House II.java
+++ Paint House II.java
@@ -1,40 +1,37 @@
 class Solution {
     public int minCostII(int[][] costs) {
-        // 2 indices pointing to min and second min cost
-        int min0 = -1, min1 = -1;
         int n = costs.length;
         if(n == 0)  return 0;
         int k = costs[0].length;
-        int[][] dp = new int[n][k];
+        int[][] dp = new int[n+1][k];
         
+        // indices of min and 2nd-min; need in total 4 pointers, last round(last1, last2) and this round(m1, m2)
+        int m1 = -1, m2 = -1;
         for(int i = 0; i < n; i++) {
-            int m0 = min0, m1 = min1;
-            min0 = -1;
-            min1 = -1;
-            
+            // new and reset
+            int last1 = m1, last2 = m2;
+            m1 = -1;
+            m2 = -1;
             for(int j = 0; j < k; j++) {
-                // update sums
-                if(j != m0) {
-                    // first iteration
-                    if(m0 < 0)  dp[i][j] = costs[i][j];
-                    else    dp[i][j] = dp[i-1][m0] + costs[i][j];
+                dp[i+1][j] += costs[i][j];
+                // update dp, cautious about -1 ptr
+                if(j == last1) {
+                    if(last2 >= 0) dp[i+1][j] += dp[i][last2];
                 }
-                else {
-                    // first iteration
-                    if(m1 < 0)  dp[i][j] = costs[i][j];
-                    else    dp[i][j] = dp[i-1][m1] + costs[i][j];
+                else{
+                    if(last1 >= 0) dp[i+1][j] += dp[i][last1];
                 }
                 
-                // update min indices
-                if(min0 < 0 || dp[i][j] < dp[i][min0]) {
-                    min1 = min0;
-                    min0 = j;
+                // update indices, initial force update
+                if(m1<0 || dp[i+1][j] < dp[i+1][m1]) {
+                    m2 = m1;
+                    m1 = j;
                 }
-                else if(min1 < 0 || dp[i][j] < dp[i][min1]) {
-                    min1 = j;
+                else if(m2<0 || dp[i+1][j] < dp[i+1][m2]) {
+                    m2 = j;
                 }
             }
         }
-        return dp[n-1][min0];
+        return dp[n][m1];
     }
 }
