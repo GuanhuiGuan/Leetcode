774473224e7eb3f4748b3d8de860b73be8d34a5c
Guanhui Guan <guanhui.guan@columbia.edu>
2018-01-22 16:32:14 -0500

    Consider duplicate data structure

--- Shortest Word Distance II.java
+++ Shortest Word Distance II.java
@@ -1,39 +1,39 @@
 class WordDistance {
-    private Map<String, List<Integer>> map;
-    private int n;
+    Map<String, List<Integer>> map = new HashMap<>();
 
     public WordDistance(String[] words) {
-        n = words.length;
-        map = new HashMap<>();
-        for(int i = 0; i < words.length; i++){
-            if(map.containsKey(words[i])){
-                map.get(words[i]).add(i);
-            }
-            else{
-                List<Integer> newL = new ArrayList<>();
-                newL.add(i);
-                map.put(words[i], newL);
-            }
+        for(int i = 0; i < words.length; i++) {
+            List<Integer> list = new ArrayList<>();
+            if(map.containsKey(words[i]))   list = map.get(words[i]);
+            list.add(i);
+            map.put(words[i], list);
         }
     }
     
     public int shortest(String word1, String word2) {
-        int minL = n+1;
-        List<Integer> l1 = map.get(word1);
-        List<Integer> l2 = map.get(word2);
+        List<Integer> l1 = map.get(word1), l2 = map.get(word2);
+        int min = Integer.MAX_VALUE, i = 0, j = 0;
         
-        for(int i = 0, j = 0; i < l1.size() && j < l2.size();){
-            int v1 = l1.get(i), v2 = l2.get(j);
-            if(v1 < v2){
-                minL = Math.min(minL, v2 - v1);
+        if(word1.equals(word2)) {
+            while(i < l1.size()-1) {
+                min = Math.min(min, l1.get(i+1)-l1.get(i));
                 i++;
             }
-            else{
-                minL = Math.min(minL, v1 - v2);
-                j++;
+        }
+        else {
+            while(i < l1.size() && j < l2.size()) {
+                int v1 = l1.get(i), v2 = l2.get(j);
+                if(v1 < v2) {
+                    min = Math.min(min, v2-v1);
+                    i++;
+                }
+                else {
+                    min = Math.min(min, v1-v2);
+                    j++;
+                }
             }
         }
-        return minL;
+        return min;
     }
 }
 
