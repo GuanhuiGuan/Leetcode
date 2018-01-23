df57e000ab0b88a75ef1744d9ada1142bbc67ed0
Guanhui Guan <guanhui.guan@columbia.edu>
2018-01-23 01:04:27 -0500

    Math, switch odd times

--- Bulb Switcher.java
+++ Bulb Switcher.java
@@ -1,6 +1,9 @@
 class Solution {
     public int bulbSwitch(int n) {
-        // bulbs at square numbers must be on, others are off
-        return (int)Math.sqrt(n);
+        // Math
+        // for Prime nums, they're only switched twice(1st and theirs), off
+        // others are on if switched odd times(1st, theirs, plus odd num)
+        // those with sqrt would work, only those x*x <= n
+        return (int)(Math.sqrt(n));
     }
 }
