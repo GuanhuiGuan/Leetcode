5370309ffae9d5a9804bb480b261d14128f2856b
Guanhui Guan <guanhui.guan@columbia.edu>
2018-01-23 16:43:14 -0500

    Bit to improve space complexity

--- Repeated DNA Sequences.java
+++ Repeated DNA Sequences.java
@@ -1,34 +1,42 @@
 class Solution {
     public List<String> findRepeatedDnaSequences(String s) {
-        // use set to deal with duplicates
-        Set pre = new HashSet(), re = new HashSet();
-        for(int i = 0; i+9 < s.length(); i++){
-            String sub = s.substring(i, i+10);
-            if(!pre.add(sub)){
-                re.add(sub);
-            }
-        }
-        return new ArrayList(re);
-    }
-}
-
-// class Solution {
-//     public List<String> findRepeatedDnaSequences(String s) {
-//         List<String> list = new ArrayList<>();
-//         Map<String, Integer> map = new HashMap<>();
-//         if(s.length() < 10) return list;
+        // transfer them into bits to save space
+        Set<Integer> seq = new HashSet<>(), dpcSeq = new HashSet<>();
+        List<String> re = new ArrayList<>();
+        int[] map = new int[26];
+        map['A'-'A'] = 0;
+        map['C'-'A'] = 1;
+        map['G'-'A'] = 2;
+        map['T'-'A'] = 3;
         
+        // left shift 2 every time
+        int i = 0, j = 0;
+        for(; i < s.length()-9; i++) {
+            int bit = 0;
+            for(j = 0; j < 10; j++) {
+                bit |= map[s.charAt(i+j)-'A'];
+                bit <<= 2;
+            }
+            if(!seq.add(bit) && dpcSeq.add(bit))    re.add(s.substring(i, i+j));
+        }
+        return re;
+        
+        
+//         // not optimal
+//         List<String> re = new ArrayList<>();
 //         int n = s.length();
-//         for(int i = 0; i+9 < n; i++){
+//         if(n < 10)  return re;
+//         Set<String> set = new HashSet<>();
+        
+//         for(int i = 0; i+9 < n; i++) {
 //             String sub = s.substring(i, i+10);
-//             if(map.containsKey(sub) && map.get(sub) == 0){
-//                 list.add(sub);
-//                 map.put(sub, 1);
-//             }
-//             else if(!map.containsKey(sub)){
-//                 map.put(sub, 0);
+//             // if duplicate
+//             if(!set.add(sub)) {
+//                 // check if already added
+//                 if(!re.contains(sub))   re.add(sub);
 //             }
 //         }
-//         return list;
-//     }
-// }
+        
+//         return re;
+    }
+}
