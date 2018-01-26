f7dbbe558e883d03dbca1bda59f74c708d690529
Guanhui Guan <guanhui.guan@columbia.edu>
2018-01-25 21:35:32 -0500

    DP, split at every node

--- Unique Binary Search Trees.java
+++ Unique Binary Search Trees.java
@@ -1,10 +1,14 @@
 class Solution {
     public int numTrees(int n) {
-        // treat every num as root
-        // sum products of left and right part
+        // dp as #uniBST for n nodes
+        // dp[n] = sum_for_i(dp[i-1]*dp[n-i])
+        if(n == 0 || n == 1)  return 1;
         int[] dp = new int[n+1];
         dp[0] = 1;
-        for(int i = 1; i <= n; i++) {
+        dp[1] = 1;
+        
+        for(int i = 2; i <= n; i++) {
+            // j means pick the 1->i-th node as root
             for(int j = 1; j <= i; j++) {
                 dp[i] += dp[j-1]*dp[i-j];
             }
