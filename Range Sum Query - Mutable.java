59faafba8c7d0b1657924d0dab2e464ea6299cdb
Guanhui Guan <guanhui.guan@columbia.edu>
2018-01-28 00:05:21 -0500

    Binary Indexed Tree - O(mlogn) time and less space

--- Range Sum Query - Mutable.java
+++ Range Sum Query - Mutable.java
@@ -1,74 +1,114 @@
+// ___________________Binary Indexed Tree Solution____________________
 class NumArray {
-    // segment tree
-    STNode root;
+    int[] BIT, nums;
+    int n;
 
     public NumArray(int[] nums) {
-        root = build(0, nums.length-1, nums);
+        n = nums.length;
+        this.nums = new int[n];
+        BIT = new int[n+1];
+        for(int i = 0; i < n; i++) {
+            update(i, nums[i]);
+        }
     }
     
+    // get delta, update num with val, update BIT with delta, move by i+(i&(-i))
     public void update(int i, int val) {
-        udNode(root, i, val);
+        int delta = val - nums[i];
+        nums[i] = val;
+        // update BIT
+        i++;
+        while(i <= n) {
+            BIT[i] += delta;
+            i += (i & (-i));
+        }
     }
     
     public int sumRange(int i, int j) {
-        return sumRange(root, i, j);
+        return BITsum(j) - BITsum(i-1);
     }
     
-    // ________________segment tree and helpers________________
-    class STNode {
-        int start, end;
-        STNode left = null, right = null;
+    // BIT sum helper, sum 0 -> i
+    public int BITsum(int i) {
         int sum = 0;
-        public STNode(int s, int e) {
-            start = s;
-            end = e;
+        i++;
+        while(i > 0) {
+            sum += BIT[i];
+            i -= (i & (-i));
         }
-    }
-    
-    public STNode build(int start, int end, int[] nums) {
-        if(start > end) return null;
-        
-        STNode node = new STNode(start, end);
-        if(start == end){
-            node.sum = nums[start];
-        }
-        else{
-            int mid = start + (end-start)/2; //avoid overflow
-            node.left = build(start, mid, nums);
-            node.right = build(mid+1, end, nums);
-            node.sum = node.left.sum + node.right.sum;
-        }
-        return node;
-    }
-    
-    public void udNode(STNode node, int i, int val){
-        if(node == null || i < node.start || i > node.end)  return;
-        if(node.start == node.end){
-            node.sum = val;
-        }
-        else{
-            int mid = node.start + (node.end-node.start)/2;
-            if(i <= mid)    udNode(node.left, i, val);
-            else    udNode(node.right, i, val);
-            
-            node.sum = node.left.sum + node.right.sum;
-        }
-    }
-    
-    public int sumRange(STNode node, int i, int j){
-        if(i > j || node == null)   return 0;
-        if(node.start == i && node.end == j)    return node.sum;
-        
-        int mid = node.start + (node.end-node.start)/2;
-        if(j <= mid) return sumRange(node.left, i, j);
-        if(i > mid)   return sumRange(node.right, i, j);
-        return sumRange(node.left, i, mid) + sumRange(node.right, mid+1, j);
+        return sum;
     }
 }
 
-/**
- * Your NumArray object will be instantiated and called as such:
- * NumArray obj = new NumArray(nums);
- * obj.update(i,val);
- * int param_2 = obj.sumRange(i,j);
- */
+// // ___________________Segment Tree Solution_____________________
+// class NumArray_SegmentTree {
+//     // Segment tree(Augmenting tree)
+//     SegTNode root;
+
+//     public NumArray(int[] nums) {
+//         root = build(0, nums.length-1, nums);
+//     }
+    
+//     public void update(int i, int val) {
+//         upd(i, val, root);
+//     }
+    
+//     public int sumRange(int i, int j) {
+//         return range(i, j, root);
+//     }
+    
+//     // _____________Segment tree helpers_______________
+//     class SegTNode {
+//         int start, end;
+//         int sum = 0;
+//         SegTNode left, right;
+//         public SegTNode(int s, int e) {
+//             start = s;
+//             end = e;
+//         }
+//     }
+    
+//     public SegTNode build(int i, int j, int[] nums) {
+//         if(i > j)   return null;
+//         SegTNode node = new SegTNode(i, j);
+//         if(i == j) {
+//             node.sum = nums[i];
+//         }
+//         else {
+//             int mid = i + (j-i)/2;
+//             node.left = build(i, mid, nums);
+//             node.right = build(mid+1, j, nums);
+//             node.sum = node.left.sum + node.right.sum;
+//         }
+//         return node;
+//     }
+    
+//     public void upd(int i, int val, SegTNode node) {
+//         if(node == null)    return;
+//         if(i < node.start || i > node.end)  return;
+//         if(node.start == node.end && node.start == i)   node.sum = val;
+//         else {
+//             int mid = node.start + (node.end-node.start)/2;
+//             if(i <= mid) upd(i, val, node.left);
+//             else    upd(i, val, node.right);
+//             node.sum = node.left.sum + node.right.sum;
+//         }
+//     }
+    
+//     public int range(int i, int j, SegTNode node) {
+//         if(i > j)   return 0;
+//         if(node == null)    return 0;
+//         if(node.start == i && node.end == j)    return node.sum;
+//         int mid = node.start + (node.end-node.start)/2;
+//         if(j <= mid)    return range(i, j, node.left);
+//         if(i > mid) return range(i, j, node.right);
+//         return range(i, mid, node.left) + range(mid+1, j, node.right);
+//     }
+// }
+
+// /**
+//  * Your NumArray object will be instantiated and called as such:
+//  * NumArray obj = new NumArray(nums);
+//  * obj.update(i,val);
+//  * int param_2 = obj.sumRange(i,j);
+//  */
