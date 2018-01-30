4f7f2133c930d9550ad3988134238e6a7f8f6403
Guanhui Guan <guanhui.guan@columbia.edu>
2018-01-29 20:48:04 -0500

    Binary Search

--- Guess Number Higher or Lower.java
+++ Guess Number Higher or Lower.java
@@ -5,14 +5,15 @@
 
 public class Solution extends GuessGame {
     public int guessNumber(int n) {
-        int start = 1, end = n;
-        while(start <= end){
-            int mid = start + (end - start)/2;
-            int result = guess(mid);
-            if(result == 0) return mid;
-            if(result < 0)   end = mid-1;
-            else    start = mid+1;
+        int lo = 1, hi = n;
+        while(lo <= hi) {
+            int mid = lo + (hi-lo)/2;
+            if(guess(mid) == 0)  return mid;
+            // ori num is greater
+            else if(guess(mid) > 0)    lo = mid+1;
+            // ori num is smaller
+            else if(guess(mid) < 0)   hi = mid-1;
         }
-        return start;
+        return lo;
     }
 }
