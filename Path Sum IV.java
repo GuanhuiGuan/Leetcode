e9250c8b68c5979d11498c18ea4a7bf10b557674
Guanhui Guan <guanhui.guan@columbia.edu>
2018-02-02 18:12:41 -0500

    Build tree with map; Traverse list again to update children level value; Collect leaves

--- Path Sum IV.java
+++ Path Sum IV.java
@@ -1,3 +1,32 @@
+// Iterative method
+class Solution {
+    public int pathSum(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        // initialize tree in a map
+        for(int num: nums) {
+            int key = num/10, val = num%10;
+            map.put(key, val);
+        }
+        
+        // iterate again, update value from top to bottom in map(lvl to lvl walk)
+        int sum = 0;
+        for(int num: nums) {
+            int key = num/10, val = num%10;
+            int lvl = key/10, pos = key%10;
+            // get key to potential children
+            int lc = (lvl+1)*10 + 2*pos-1, rc = (lvl+1)*10 + 2*pos;
+            // if any child exist, add value
+            if(map.containsKey(lc)) map.put(lc, map.get(lc)+map.get(key));
+            if(map.containsKey(rc)) map.put(rc, map.get(rc)+map.get(key));
+            // no children, collect leaves
+            if(!map.containsKey(lc) && !map.containsKey(rc))    sum += map.get(key);
+        }
+        return sum;
+    }
+}
+
+// Recursion method
 class Solution {
     int sum = 0;
     
