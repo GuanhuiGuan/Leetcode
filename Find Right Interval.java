ce9abcedb00be7b0e092de509e932df824501c71
Guanhui Guan <guanhui.guan@columbia.edu>
2018-02-23 02:17:27 -0500

    TreeMap

---
+++ Find Right Interval.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for an interval.
+ * public class Interval {
+ *     int start;
+ *     int end;
+ *     Interval() { start = 0; end = 0; }
+ *     Interval(int s, int e) { start = s; end = e; }
+ * }
+ */
+class Solution {
+    public int[] findRightInterval(Interval[] intervals) {
+        // save the start point and the index
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        for(int k = 0; k < intervals.length; k++) {
+            map.put(intervals[k].start, k);
+        }
+        
+        int[] res = new int[intervals.length];
+        for(int k = 0; k < intervals.length; k++) {
+            Integer right = map.ceilingKey(intervals[k].end);
+            res[k] = right==null? -1: map.get(right);
+        }
+        
+        return res;
+    }
+}
