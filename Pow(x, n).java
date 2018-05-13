1e5d5a9fd4ebc751b0211c336fbfc13da1fac077
Guanhui Guan <guanhui.guan@columbia.edu>
2018-05-13 11:33:17 +0800

    Recursion; Odd/Even

--- Pow(x, n).java
+++ Pow(x, n).java
@@ -1,19 +1,19 @@
 class Solution {
     public double myPow(double x, int n) {
-        if(x == 0)  return 0;
-        if(n == 0)  return 1;
-        if(n < 0){
-            // check if overflow
-            if(n == Integer.MIN_VALUE){
-                n++;
+        if(n == 0) return 1.0;
+        if(n == 1) return x;
+        
+        if(n < 0) {
+            if(n < 0) {
                 n = -n;
-                x = 1/x;
-                return n%2 == 1 ? myPow(x*x, n/2)*x*x : myPow(x*x, n/2)*x;
+                // if not overflow
+                if(n >= 0) {
+                    x = 1/x;
+                }
             }
-            
-            n = -n;
-            x = 1/x;
         }
-        return n%2 == 1 ? myPow(x*x, n/2)*x : myPow(x*x, n/2);
+        
+        double prod = myPow(x * x, n/2);
+        return n%2 == 0? prod: prod * x;
     }
 }
