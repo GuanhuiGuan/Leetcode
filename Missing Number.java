bb117d82bc6ae368099f8640514f7687300c043c
Guanhui Guan <guanhui.guan@columbia.edu>
2018-06-01 18:08:51 -0400

    Xor

--- Missing Number.java
+++ Missing Number.java
@@ -1,9 +1,10 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int s = nums.length;
-        for(int i = 0; i < nums.length; i++) {
-            s ^= i ^ nums[i];
+        // xor
+        int n = nums.length, x = n;
+        for(int i = 0; i < n; i++) {
+            x ^= i ^ nums[i];
         }
-        return s;
+        return x;
     }
 }
