bdcd5cc606acf41314a229d250ecfce236de3c13
Guanhui Guan <guanhui.guan@columbia.edu>
2018-06-01 20:05:06 -0400

    num1[i] * num2[j] --> num[i + j], num[i + j + 1]

--- Multiply Strings.java
+++ Multiply Strings.java
@@ -1,25 +1,26 @@
 class Solution {
     public String multiply(String num1, String num2) {
-        /* ref: https://leetcode.com/problems/multiply-strings/discuss/17605/ */
-        // product of pair n1[i], n2[j] is at n[i+j, i+j+1]
-        int m = num1.length(), n = num2.length();
-        int[] num = new int[m + n];
+        // num1[i] * num2[j] --> num[i + j], num[i + j + 1]
+        int[] num = new int[num1.length() + num2.length()];
         
-        for(int i = m-1; i >= 0; i--){
-            for(int j = n-1; j >= 0; j--){
-                int pro = (num1.charAt(i) - '0')*(num2.charAt(j) - '0');
-                pro += num[i+j+1];
-                num[i+j] += pro/10;
-                num[i+j+1] = pro%10;
+        for(int i = num1.length() - 1; i >= 0; i--) {
+            for(int j = num2.length() - 1; j >= 0; j--) {
+                int t = (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
+                int lower = t + num[i + j + 1];
+                num[i + j + 1] = lower % 10;
+                num[i + j] += lower / 10;
             }
         }
-        StringBuilder sb = new StringBuilder();
-        for(int d: num){
-            // avoid x*0 = 00...00
-            if(!(sb.length()==0 && d == 0)){
-                sb.append(d);
-            }
+        
+        return toString(num);
+    }
+    
+    public String toString(int[] num) {
+        StringBuilder str = new StringBuilder();
+        for(int i = 0; i < num.length; i++) {
+            if(str.length() == 0 && num[i] == 0)    continue;
+            str.append(num[i]);
         }
-        return sb.length() == 0? "0": sb.toString();
+        return str.length() == 0? "0": str.toString();
     }
 }
