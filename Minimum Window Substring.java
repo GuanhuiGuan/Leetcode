68a1d54b08a902080da509fa6f67225814cfb541
Guanhui Guan <guanhui.guan@columbia.edu>
2018-06-03 17:13:59 -0400

    Sliding Window Double Loop; Check best res first in inner loop

--- Minimum Window Substring.java
+++ Minimum Window Substring.java
@@ -1,36 +1,35 @@
 class Solution {
     public String minWindow(String s, String t) {
-        // substring problem: map, count && 2ptrs
-        // outer loop: check if key exists, and check if count changes
-        // inner loop: update min substring, check if key exists and check if count changes
-        Map<Character, Integer> map = new HashMap<>();
-        for(int k = 0; k < t.length(); k++) {
-            map.put(t.charAt(k), map.containsKey(t.charAt(k))? map.get(t.charAt(k))+1: 1);
-        }
-        int i = 0, count = t.length(), res = 0, relen = Integer.MAX_VALUE;
+        int[] map = new int[256];
+        int k = t.length();
         
-        for(int j = 0; j < s.length(); j++) {
-            char c = s.charAt(j);
-            if(map.containsKey(c)) {
-                if(map.get(c) > 0)  count--;
-                map.put(c, map.get(c)-1);
-                
-                while(count == 0) {
-                    if(relen > j-i+1) {
-                        res = i;
-                        relen = j-i+1;
-                    }
-                    char prev = s.charAt(i);
-                    if(map.containsKey(prev)) {
-                        if(map.get(prev) == 0)  count++;
-                        map.put(prev, map.get(prev)+1);
-                    }
-                    i++;
+        initMap(map, t);
+        
+        int i = 0, j = 0, len = Integer.MAX_VALUE, start = 0;
+        while(j < s.length()) {
+            if(map[s.charAt(j++)]-- > 0) {
+                k--;
+            }
+            
+            while(k == 0) {
+                // check if it's best result first
+                if(len > j - i) {
+                    // now j has already incremented, so no need to plus 1
+                    len = j - i;
+                    start = i;
+                }
+                // move i forward
+                if(map[s.charAt(i++)]++ == 0) {
+                    k++;
                 }
             }
         }
-        
-        if(relen == Integer.MAX_VALUE)  return "";
-        return s.substring(res, res+relen);
+        return len == Integer.MAX_VALUE? "": s.substring(start, start + len);
+    }
+    
+    public void initMap(int[] map, String t) {
+        for(int i = 0; i < t.length(); i++) {
+            map[t.charAt(i)]++;
+        }
     }
 }
