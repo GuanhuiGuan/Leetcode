70f0532d10480dbb7d0f66b7db2c823e76bd7e86
Guanhui Guan <guanhui.guan@columbia.edu>
2018-06-08 23:05:03 -0400

    DP from tail

--- Decode Ways.java
+++ Decode Ways.java
@@ -1,28 +1,26 @@
 class Solution {
     public int numDecodings(String s) {
+        if(s == null || s.length() == 0)    return 0;
         int n = s.length();
+        
+        // DP from the tail
+        // init state
         int[] dp = new int[n+1];
-        
-        if(n == 0)  return 0;
-        // empty string
         dp[n] = 1;
-        // 1 character
-        if(s.charAt(n-1) == '0'){
-            dp[n-1] = 0;
-        }
-        else{
-            dp[n-1] = 1;
-        }
-        // backwards
-        for(int i = n-2; i >= 0; i--){
-            // skip those start with 0, cause it can't be decoded
+        dp[n-1] = s.charAt(n-1) == '0'? 0: 1;
+        
+        // usual DP
+        for(int i = n-2; i >= 0; i--) {
+            // cannot start with 0 for a pair
             if(s.charAt(i) == '0')  continue;
-            // substring <= 26, two way, otherwise one way
-            dp[i] = (Integer.parseInt(s.substring(i, i+2)) <= 26) ?
-                dp[i+1] + dp[i+2] : dp[i+1];
+            String sub = s.substring(i, i+2);
+            dp[i] += valid(sub)? dp[i+1] + dp[i+2]: dp[i+1];
         }
-        
-        
         return dp[0];
+    }
+    
+    public boolean valid(String s) {
+        int num = Integer.parseInt(s);
+        return (num > 0 && num < 27);
     }
 }
