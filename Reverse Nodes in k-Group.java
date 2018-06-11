d17a34e01965ca7157dac646a2261c8096454f78
Guanhui Guan <guanhui.guan@columbia.edu>
2018-06-10 22:46:03 -0400

    recursion; Split the list

--- Reverse Nodes in k-Group.java
+++ Reverse Nodes in k-Group.java
@@ -8,28 +8,49 @@
  */
 class Solution {
     public ListNode reverseKGroup(ListNode head, int k) {
-        ListNode dr = new ListNode(0);
-        dr.next = head;
-        
-        // find the length of the list
-        int n = 0;
-        for(ListNode end = head; end != null; n++, end = end.next);
-        // n is the length
-        
-        // reverse all sublists of length k
-        for(ListNode prev = dr, cur = head, next = cur.next; n >= k; n -= k) {
-            // only k-1 reversions
-            for(int i = 1; i < k; i++) {
-                cur.next = next.next;
-                next.next = prev.next;
-                prev.next = next;
-                next = cur.next;
-            }
-            prev = cur;
-            cur = cur.next;
-            next = cur.next;
+        // find the k-size sublist
+        if(head == null)    return head;
+        ListNode node = head;
+        int i = 1;
+        for(; node.next != null && i < k; i++) {
+            node = node.next;
         }
+        if(i < k)   return head;
         
+        // split and reverse current, recurse on the latter list
+        ListNode next = node.next;
+        node.next = null;
+        ListNode newHead = reverse(head), newTail = reverseKGroup(next, k);
+        // ListNode newHead = reverse(head, null), newTail = reverseKGroup(next, k);
+        
+        // reconnect
+        ListNode newHeadEnd = findEnd(head);
+        newHeadEnd.next = newTail;
+        return newHead;
+    }
+    
+    public ListNode reverse(ListNode head) {
+        // iterative
+        ListNode dr = new ListNode(0);
+        while(head != null) {
+            ListNode next = head.next;
+            head.next = dr.next;
+            dr.next = head;
+            head = next;
+        }
         return dr.next;
+    }
+    
+    public ListNode reverse(ListNode head, ListNode newTail) {
+        // recursive
+        if(head == null)    return newTail;
+        ListNode next = head.next;
+        head.next = newTail;
+        return reverse(next, head);
+    }
+    
+    public ListNode findEnd(ListNode head) {
+        while(head.next != null)    head = head.next;
+        return head;
     }
 }
