46e9b676cdae4dccab2d8734b5586314eb16b813
Guanhui Guan <gg2685@columbia.edu>
2018-07-15 21:55:12 -0400

    Store the timestamp+10 in map

---
+++ Logger Rate Limiter.java
@@ -0,0 +1,24 @@
+class Logger {
+    
+    private Map<String, Integer> lastTS;
+
+    /** Initialize your data structure here. */
+    public Logger() {
+        lastTS = new HashMap<>();
+    }
+    
+    /** Returns true if the message should be printed in the given timestamp, otherwise returns false.
+        If this method returns false, the message will not be printed.
+        The timestamp is in seconds granularity. */
+    public boolean shouldPrintMessage(int timestamp, String message) {
+        if(timestamp < lastTS.getOrDefault(message, 0))    return false;
+        lastTS.put(message, timestamp + 10);
+        return true;
+    }
+}
+
+/**
+ * Your Logger object will be instantiated and called as such:
+ * Logger obj = new Logger();
+ * boolean param_1 = obj.shouldPrintMessage(timestamp,message);
+ */
