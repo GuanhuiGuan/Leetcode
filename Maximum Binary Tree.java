0aceb83d3009a041e06fd9d294468528c1c00297
Guanhui Guan <gg2685@columbia.edu>
2018-07-23 22:33:27 -0400

    Deque to store desc nodes, loop pop for left child, connect as right child, insert; Or use divide

--- Maximum Binary Tree.java
+++ Maximum Binary Tree.java
@@ -9,27 +9,42 @@
  */
 class Solution {
     public TreeNode constructMaximumBinaryTree(int[] nums) {
-        if(nums.length == 0)    return null;
-        int index = 0;
-        for(int i = 0; i < nums.length; i++) {
-            if(nums[index] < nums[i]) {
+        // return divide(nums, 0, nums.length-1);
+        return deque(nums);
+    }
+    
+    private TreeNode deque(int[] nums) {
+        // O(n), deque stores nodes with descending values
+        Deque<TreeNode> deq = new LinkedList<>();
+        for(int num: nums) {
+            TreeNode node = new TreeNode(num);
+            
+            // Get the max possible left node(earlier popped nodes are already connected to even earlier nodes)
+            while(!deq.isEmpty() && deq.peek().val < num)   node.left = deq.pop();
+            
+            // If there're still earlier (greater) nodes, link current node
+            if(!deq.isEmpty())  deq.peek().right = node;
+            
+            // Insert current node
+            deq.push(node);
+        }
+        return deq.isEmpty()? null: deq.removeLast();
+    }
+    
+    private TreeNode divide(int[] nums, int s, int e) {
+        if(s > e)   return null;
+        if(s == e)  return new TreeNode(nums[s]);
+        
+        int index = 0, max = Integer.MIN_VALUE;
+        for(int i = s; i <= e; i++) {
+            if(nums[i] > max) {
+                max = nums[i];
                 index = i;
             }
         }
-        TreeNode node = new TreeNode(nums[index]);
-        TreeNode ln = constructMaximumBinaryTree(getSubArray(nums, 0, index));
-        TreeNode rn = constructMaximumBinaryTree(getSubArray(nums, index+1, nums.length));
-        node.left = ln;
-        node.right = rn;
+        TreeNode node = new TreeNode(max);
+        node.left = divide(nums, s, index-1);
+        node.right = divide(nums, index+1, e);
         return node;
-    }
-    
-    public int[] getSubArray(int[] nums, int i, int j) {
-        int n = j-i;
-        int[] re = new int[n];
-        for(int k = 0; k < n; k++) {
-            re[k] = nums[i+k];
-        }
-        return re;
     }
 }
